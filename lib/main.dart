89c74c8d523eb34125a322bf75bf5ea2d8a5b871
Far14z <faridce14@gmail.com>
2026-06-06 12:23:18 -0500

    feat(deployment): configure firebase integration and update dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:restock/shared/infrastructure/services/auth_status_notifier.dart';
 import './injections.dart' as di;
 import 'package:restock/shared/infrastructure/navigation/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 /// The main function is the entry point of the application. It initializes the dependency injection and runs the app.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.setupDependencies();  
   await di.serviceLocator<AuthStatusNotifier>().initialize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const RestockApp());
 }
 
